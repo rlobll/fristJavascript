3d671fbb97e947f8a53534837742d1277224ed68
eunbi <dmsql3335@gmail.com>
2022-05-09 21:11:27 +0900

    as

--- javascript.js
+++ javascript.js
@@ -2,3 +2,4 @@ document.getElementById("demo").innerHTML = "6월16일";
 document.getElementById("demo").style.color = "blue";
 document.getElementById("demo2").innerHTML = Date();
 document.getElementById("demo").innerHTML;
+p;
