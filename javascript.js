5be0562b2604ec4633453bb643fa0fc5d5e20b99
eunbi <dmsql3335@gmail.com>
2022-05-09 21:07:13 +0900

    second commit

---
+++ javascript.js
@@ -0,0 +1,4 @@
+document.getElementById("demo").innerHTML = "6월16일";
+document.getElementById("demo").style.color = "blue";
+document.getElementById("demo2").innerHTML = Date();
+document.getElementById("demo").innerHTML;
